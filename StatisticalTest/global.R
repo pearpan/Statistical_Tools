a19a7eb6f32166d4618e5a57610c5b690bdf762c
pearpan <lpan@imvu.com>
2015-01-29 17:05:03 -0800

    do not install not useful package

--- StatisticalTest/global.R
+++ StatisticalTest/global.R
@@ -1,12 +1,11 @@
 #options(shiny.trace = TRUE)
 
 #check for and/or install dependencies
-need<-c("shiny","googleVis") # some more should auto load...
+need<-c("shiny") # some more should auto load...
 for(i in 1:length(need)){
   if(require(need[i], character.only = TRUE)==FALSE){install.packages(need[i]);library(need[i], character.only = TRUE)} else { library(need[i],character.only = TRUE)}
 }
 
-library(googleVis)
 library(shiny)
 
 
